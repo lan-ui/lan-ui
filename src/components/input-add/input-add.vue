c3cda0d9c9f432b7bf0d00b8e9a68aaa87821a3d
xingweibo <3152360788@qq.com>
2020-06-02 13:50:06 +0800

    icon-lan修改

--- src/components/input-add/input-add.vue
+++ src/components/input-add/input-add.vue
@@ -164,8 +164,8 @@
         return eye.reverse ? !eye.open : eye.open
       },
       eyeClass() {
-        return this.formatedEye.open ? 'lan-icon-yanjing-keyikan' : 'lan-icon-bukejian1'
-        // return this.formatedEye.open ? 'icon-hualife-visible' : 'icon-hualife-invisible'
+        // return this.formatedEye.open ? 'lan-icon-yanjing-keyikan' : 'lan-icon-bukejian1'
+        return this.formatedEye.open ? 'icon-lan-visible' : 'icon-lan-invisible'
       }
       // inputValueFormat: {
       //   get() {
